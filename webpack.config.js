d10f57756a1b5439b0d0c85f446ce5bb790f6e88
Igor Kostyakov <i.kostyakov@dunice.net>
2020-04-30 20:40:57 +0300

    Configure webpack, eslint, jest

--- webpack.config.js
+++ webpack.config.js
@@ -1,30 +1,38 @@
+// eslint-disable-next-line @typescript-eslint/no-var-requires
 const path = require('path');
 
-module.exports = [{
-  entry: './src/index.ts',
-  mode: 'development',
-  devtool: 'inline-source-map',
-  devServer: {
-    contentBase: './dist',
-  },
-  module: {
-    rules: [
-      {
-        test: /\.tsx?$/,
-        use: 'ts-loader',
-        exclude: /node_modules/,
+module.exports = [
+  {
+    entry: './src/uiThread/index.ts',
+    mode: 'development',
+    devtool: 'inline-source-map',
+    devServer: {
+      contentBase: './dist',
+    },
+    module: {
+      rules: [
+        {
+          test: /\.tsx?$/,
+          use: 'ts-loader',
+          exclude: /node_modules/,
+        },
+      ],
+    },
+    resolve: {
+      extensions: ['.tsx', '.ts', '.js'],
+      alias: {
+        '@/common': path.resolve(__dirname, 'src/common/'),
+        '@/logicThread': path.resolve(__dirname, 'src/logicThread/'),
+        '@/uiThread': path.resolve(__dirname, 'src/uiThread/'),
       },
-    ],
+    },
+    output: {
+      filename: 'uiThread.js',
+      path: path.resolve(__dirname, 'dist'),
+    },
   },
-  resolve: {
-    extensions: ['.tsx', '.ts', '.js'],
-  },
-  output: {
-    filename: 'index.js',
-    path: path.resolve(__dirname, 'dist'),
-  },
-}, {
-    entry: './src/worker.ts',
+  {
+    entry: './src/logicThread/index.ts',
     mode: 'development',
     devtool: 'inline-source-map',
     module: {
@@ -38,9 +46,15 @@ module.exports = [{
     },
     resolve: {
       extensions: ['.tsx', '.ts', '.js'],
+      alias: {
+        '@/common': path.resolve(__dirname, 'src/common/'),
+        '@/logicThread': path.resolve(__dirname, 'src/logicThread/'),
+        '@/uiThread': path.resolve(__dirname, 'src/uiThread/'),
+      },
     },
     output: {
-      filename: 'worker.js',
+      filename: 'logicThread.js',
       path: path.resolve(__dirname, 'dist'),
     },
-  }];
+  },
+];
